5fe0db4b06f0de97d1b80a386b96b051fec3ced9
Millin Gabani <gabanimillin@gmail.com>
2018-04-14 05:09:34 -0700

    Graph-wip

--- places/places_handler.py
+++ places/places_handler.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 import requests
 import json
-
+from igraph import *fo
 
 class PlacesHandler:
     def __init__(self):
@@ -22,4 +22,36 @@ class PlacesHandler:
         print url
 
         return json.loads(requests.get(url).text)['results']
+
+    @staticmethod
+    def get_list_of_places(intermediate_points, types, query):
+        for point in intermediate_points:
+            url = PlacesHandler.BASE_URL.format(point['lat'],
+                                                point['lon'],
+                                                point['radius'],
+                                                types,
+                                                query,
+                                                PlacesHandler.API_KEY)
+
+        return json.load(requests.get(url).text)
+
+    @staticmethod
+    def find_itinerary(top_choices):
+        location_graph = PlacesHandler.generate_location_graph(top_choices)
+
+
+    @staticmethod
+    def generate_location_graph(top_choices, start_location, end_location):
+        num_vertices = len(top_choices)+2
+        location_graph = Graph.Full(num_vertices)
+        name_list = [start_location['name']]
+        for i in xrange(1,num_vertices-1):
+            name_list.append(top_choices['name'])
+        name_list.append(end_location['name'])
+        location_graph.vs['name'] = name_list
+        location_graph.vs['name'][num_vertices - 1] = end_location['name']
+        for edge in graph.es:
+            location_graph.vs[edge.source]["name"]
+
+
 
